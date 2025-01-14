625c81679c6809f77b01573a795b7f68d45f016b
ju4nf3rn4ndo <jcanol@unal.edu.co>
2025-01-13 21:28:16 -0500

    1. Excluir la estación 31 automáticamente. 2. Utilizar la fecha actual como límite superior.

--- airQ_data_adq.r
+++ airQ_data_adq.r
@@ -2,13 +2,13 @@
 library(bogotAIR)
 library(dplyr)
 
-# Definir intervalo de fechas
+# Definir intervalo de fechas dinámico
 start_date <- "01-10-2020"
 end_date <- format(Sys.time(), "%d-%m-%Y %H:%M:%S")  # Fecha y hora actual
-# end_date <- "31-12-2023"
 
-# Obtener la lista de estaciones disponibles
-estaciones <- rmcab_aqs
+
+# Obtener la lista de estaciones disponibles, excluyendo la estación 31
+estaciones <- rmcab_aqs %>% filter(code != 31)
 
 # Función para descargar datos de una estación
 descargar_datos_estacion <- function(codigo_estacion) {
@@ -46,8 +46,8 @@ if (length(datos_todas_estaciones) > 0) {
   base_datos <- bind_rows(datos_todas_estaciones)
   
   # Guardar el resultado en un archivo CSV
-  write.csv(base_datos, "datos_calidad_aire_bogota_2020_2023.csv", row.names = FALSE)
-  message("Archivo 'datos_calidad_aire_bogota_2020_2023.csv' guardado con éxito.")
+  write.csv(base_datos, "datos_calidad_aire_bogota.csv", row.names = FALSE)
+  message("Archivo 'datos_calidad_aire_bogota.csv' guardado con éxito.")
 } else {
   message("No se encontraron datos para ninguna estación en el rango de fechas especificado.")
 }
